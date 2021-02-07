7006d31aadef4c848ffa7c8d888afc34c72d550a
Fire Dragon <daniwhitlock4@gmail.com>
2021-02-06 22:26:22 -0700

    fixed error for employees, added formatting for remaining functions for updating

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,7 +18,7 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 VALUES 
-('John', 'Doe', 1, 3),
+('John', 'Doe', 1, null),
 ('Mike', 'Chan', 2, 1),
 ('Ashley', 'Rodriguez', 3, null),
 ('Kevin', 'Tupik', 4, null),
